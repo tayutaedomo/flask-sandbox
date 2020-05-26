92552b396bd9de842854f5bc622901818aeea55e
tayutaedomo <tayutaedomo@gmail.com>
2020-05-26 21:06:14 +0900

    Add basic auth

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 import os
 from flask import Flask
+from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
 APP_SETTINGS = os.getenv('APP_SETTINGS', 'config.DevelopmentConfig')
 app.config.from_object(APP_SETTINGS)
 
+auth = HTTPBasicAuth()
+
+
+@auth.verify_password
+def verify_password(username, password):
+    u = os.getenv('BASICAUTH_USERNAME', 'sandbox')
+    p = os.getenv('BASICAUTH_PASSWORD', 'flask')
+    return username == u and password == p
+
 
 @app.route('/')
+@auth.login_required
 def hello():
     return "Hello World!"
 
 
 @app.route('/<name>')
+@auth.login_required
 def hello_name(name):
     return "Hello {}!".format(name)
 
